0e5bd9bf7afafef181e2eca8536e4eac9181776f
Stephen <yoonic@gmail.com>
2019-02-10 17:11:21 -0500

    Added ng components required for extension and updated ext manifest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { PopupComponent } from './popup/popup.component';
+import { EventPageComponent } from './event-page/event-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
-
+const routes: Routes = [
+    { path: 'homepage', component: HomepageComponent },
+    { path: 'event-page', component: EventPageComponent },
+    { path: 'popup', component: PopupComponent },
+    { path: '', redirectTo: 'homepage', pathMatch: 'full' }
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { EventPageComponent } from './event-page/event-page.component';
+import { PopupComponent } from './popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    EventPageComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
