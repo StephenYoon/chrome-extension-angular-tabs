1233496cb89cb14372156c31c2dfbc6c3b1021ad
Stephen <yoonic@gmail.com>
2019-02-21 20:05:21 -0500

    Minor cleanup and continued experimentation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,21 @@ export class AppComponent implements OnInit {
   myTabs = [];
 
   ngOnInit() {
-    this.myTabs = [];
-    this.getAllTabs(this.blah);
+    this.getAllTabs(this.somehowGetTabInfo);
   }
 
-  blah(tempTabs: MyTab[]): void {
+  somehowGetTabInfo(tempTabs: []): void {
     console.log('Number of tabs in blah(): ' + tempTabs.length);
-    console.log('this.myTabs: ' + this.myTabs);
-    this.myTabs = tempTabs;
+    console.log('this.myTabs: ' + this.myTabs); // <-- this doesn't work, this.myTabs is undefined
+    this.myTabs = tempTabs; // <-- this doesn't work, this.myTabs is undefined
   }
 
-  getAllTabs(fn): void {
+  getAllTabs(customFunction): void {
     chrome.tabs.query({}, function(tabs) {
       console.log("\n/////////////////////\n");
       
-      let tempTabs: MyTab[];
-      tempTabs = [];
+      //let tempTabs: MyTab[];
+      let tempTabs = [];
       tabs.forEach(function(tab){
         console.log(tab.url);
         
@@ -38,7 +37,7 @@ export class AppComponent implements OnInit {
       });
 
       console.log('Number of tempTabs: ' + tempTabs.length);
-      fn(tempTabs);
+      customFunction(tempTabs);
     });
   };
 }
